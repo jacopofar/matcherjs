75a0906a56689b3f2d1d35a0603ffe46abeb7193
jacopo_farina <jacopo.farina@spindox.it>
2016-09-26 18:49:30 +0200

    test draft

---
+++ test/expression_splitter.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe ('TBD', function (){
+  it ('see that mocha is running properly', function () {
+    expect(1).to.equal(1);
+  });
+});
