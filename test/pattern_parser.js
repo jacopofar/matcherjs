175387ac8bf0d906656126ba041b8a02ff506d47
jacopo_farina <jacopo.farina@spindox.it>
2016-09-28 15:39:45 +0200

    add test and code to split patterns

--- test/pattern_parser.js
+++ test/pattern_parser.js
@@ -14,4 +14,14 @@ describe ('pattern parser', function () {
     assert.isNull(spl[0].type, 'the type for a plain text pattern was not null');
     assert.isNull(spl[0].parameter, 'the parameter for a plain text pattern was not null');
   });
+  it ('split patterns into parts correctly', function () {
+    const text = 'this [is:a] pattern [with:parts]!';
+    const spl = matcherjs.parse_pattern(text);
+    expect(spl.length).to.equal(5);
+    expect(spl[0].text).to.be.equal('this ');
+    expect(spl[1].text).to.be.equal('[is:a]');
+    expect(spl[2].text).to.be.equal(' pattern ');
+    expect(spl[3].text).to.be.equal('[with:parts]');
+    expect(spl[4].text).to.be.equal('!');
+  });
 });
