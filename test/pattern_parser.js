d93c73f6e81e75d3e23858df1921e157cb9b5b3f
jacopo_farina <jacopo.farina@spindox.it>
2016-09-28 21:35:09 +0200

    parse types and patterns, jsdoc

--- test/pattern_parser.js
+++ test/pattern_parser.js
@@ -24,4 +24,29 @@ describe ('pattern parser', function () {
     expect(spl[3].text).to.be.equal('[with:parts]');
     expect(spl[4].text).to.be.equal('!');
   });
+  it ('throw errors with bad-formed patterns', function () {
+    const text = 'this [is:a] broken [pattern!';
+    assert.throws(() => matcherjs.parse_pattern(text), Error, 'Square brackets are unbalanced in pattern');
+  });
+
+  it ('identifies types and parameters correctly', function () {
+    const text = 'this [is:another] pattern [with: parametric parts] and [non] parametric ones!';
+    const spl = matcherjs.parse_pattern(text);
+    expect(spl.length).to.equal(7);
+    expect(spl[0].text).to.be.equal('this ');
+    expect(spl[1].text).to.be.equal('[is:another]');
+    expect(spl[1].type).to.be.equal('is');
+    expect(spl[1].parameter).to.be.equal('another');
+
+    expect(spl[2].text).to.be.equal(' pattern ');
+
+    expect(spl[3].text).to.be.equal('[with: parametric parts]');
+    expect(spl[3].type).to.be.equal('with');
+    expect(spl[3].parameter).to.be.equal(' parametric parts');
+
+
+    expect(spl[5].text).to.be.equal('[non]');
+    expect(spl[5].type).to.be.equal('non');
+    expect(spl[5].parameter).to.be.equal('');
+  });
 });
