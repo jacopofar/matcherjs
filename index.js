2a35252de3f80efe5230055cb5a838279830ed9f
jacopo_farina <jacopo.farina@spindox.it>
2016-10-01 19:57:49 +0200

    Parser empty object

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 'use strict';
 
+const regexAnnotator = require('./lib/annotators/regex');
+const insensitiveAnnotator = require('./lib/annotators/insensitive');
+const charAnnotator = require('./lib/annotators/char');
+const multiAnnotator = require('./lib/annotators/multi');
+const tagAnnotator = require('./lib/annotators/tag');
+
+class Parser {
+  constructor() {
+    this.rules = {
+      r: regexAnnotator,
+      i: insensitiveAnnotator,
+      char: charAnnotator,
+      multi: multiAnnotator,
+      tag: tagAnnotator
+    };
+  }
+  getRule(ruleName) {
+    return this.rules[ruleName];
+  }
+}
+
 module.exports.parse_pattern = require('./lib/pattern_parser');
+module.exports.Parser = Parser;
--- test/direct_annotations.js
+++ test/direct_annotations.js
@@ -6,18 +6,20 @@ const matcherjs = require('../');
 
 describe ('built-in annotations', function () {
   it ('check that there are the built-in annotators', function () {
-    expect(matcherjs.rules['r']).to.not.be.undefined;
-    expect(matcherjs.rules['i']).to.not.be.undefined;
-    expect(matcherjs.rules['char']).to.not.be.undefined;
-    expect(matcherjs.rules['multi']).to.not.be.undefined;
-    expect(matcherjs.rules['tag']).to.not.be.undefined;
+    const matcher = new matcherjs.Parser();
+    expect(matcher.getRule('r')).to.not.be.undefined;
+    expect(matcher.getRule('i')).to.not.be.undefined;
+    expect(matcher.getRule('char')).to.not.be.undefined;
+    expect(matcher.getRule('multi')).to.not.be.undefined;
+    expect(matcher.getRule('tag')).to.not.be.undefined;
   });
   it ('check that there are not other annotaros which are not the built-in', function () {
-    expect(matcherjs.rules['hello']).to.be.undefined;
-    expect(matcherjs.rules['zebra']).to.not.be.undefined;
-    expect(matcherjs.rules['ir']).to.not.be.undefined;
-    expect(matcherjs.rules['ri']).to.not.be.undefined;
-    expect(matcherjs.rules['Char']).to.not.be.undefined;
-    expect(matcherjs.rules['MULTI']).to.not.be.undefined;
+    const matcher = new matcherjs.Parser();
+    expect(matcher.getRule('hello')).to.be.undefined;
+    expect(matcher.getRule('zebra')).to.be.undefined;
+    expect(matcher.getRule('ir')).to.be.undefined;
+    expect(matcher.getRule('ri')).to.be.undefined;
+    expect(matcher.getRule('Char')).to.be.undefined;
+    expect(matcher.getRule('MULTI')).to.be.undefined;
   });
 });
